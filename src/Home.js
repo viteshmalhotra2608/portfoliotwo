33641295209e897354d7865a9648139ccd947875
Vitesh <viteshmalhotra2608@gmail.com>
2024-01-11 16:50:35 +0530

    My change

--- src/Home.js
+++ src/Home.js
@@ -58,12 +58,6 @@ const Home = () => {
                 className="w-full h-full bg-bodyColor overflow-y-scroll scrollbar-thin scrollbar-thumb-[#646464]"
               >
                 <Sidenav />
-                <span
-                  onClick={() => setSidenav(false)}
-                  className="absolute top-0 -right-16 w-12 h-12 bg-bodyColor text-2xl text-textColor hover:text-designColor duration-300 cursor-pointer flex items-center justify-center z-50"
-                >
-                  <MdOutlineClose />
-                </span>
               </motion.div>
             </div>
           </div>
@@ -132,26 +126,6 @@ const Home = () => {
               Projects
             </span>
           </span>
-          {/* Blog Icon */}
-          <span
-            onClick={() =>
-              setAbout(false) &
-              setResume(false) &
-              setProjects(false) &
-              setBlog(true) &
-              setContact(false)
-            }
-            className={`${
-              blog
-                ? "text-designColor"
-                : "w-full h-6 text-textColor text-xl flex items-center justify-center hover:text-designColor duration-300 cursor-pointer relative group"
-            } w-full h-6 text-xl flex items-center justify-center hover:text-designColor duration-300 cursor-pointer relative group`}
-          >
-            <SiGooglechat />
-            <span className="text-black font-medium text-xs uppercase bg-designColor px-4 py-[1px] rounded-xl absolute left-0 translate-x-8 group-hover:translate-x-12 opacity-0 group-hover:opacity-100 transition-all duration-300 z-20">
-              Blog
-            </span>
-          </span>
           {/* Contact Icon */}
           <span
             onClick={() =>
@@ -172,12 +146,6 @@ const Home = () => {
               Contact
             </span>
           </span>
-          <span className="w-full h-6 text-textColor text-xl flex items-center justify-center hover:text-designColor duration-300 cursor-pointer relative group">
-            <BsTelephonePlusFill />
-            <span className="text-black font-medium text-xs uppercase bg-designColor px-4 py-[1px] rounded-xl absolute left-0 translate-x-8 group-hover:translate-x-12 opacity-0 group-hover:opacity-100 transition-all duration-300 z-20">
-              Call
-            </span>
-          </span>
         </div>
         {/* ======= Other Icons End */}
       </div>
@@ -192,7 +160,6 @@ const Home = () => {
             <About />
             <Resume />
             <Projects />
-            <Blog />
             <Contact />
           </div>
           {/* ======================== Smaller device content End ========================== */}
@@ -223,15 +190,6 @@ const Home = () => {
                 transition={{ duration: 0.5 }}
               >
                 <Projects />
-              </motion.div>
-            )}
-            {blog && (
-              <motion.div
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 0.5 }}
-              >
-                <Blog />
               </motion.div>
             )}
             {contact && (
--- src/components/about/AboutMe.js
+++ src/components/about/AboutMe.js
@@ -5,11 +5,12 @@ const AboutMe = () => {
     <div className="flex flex-col md:flex-row pb-6">
       <div className="w-full md:w-1/2 text-zinc-400 px-6 border-r-[1px] border-r-zinc-800 flex items-center">
         <div className="py-6">
-          <h2 className="font-semibold mb-1">Hello! I'm John Doe</h2>
+          <h2 className="font-semibold mb-1">Hello! I am Vitesh Malhotra</h2>
           <p className="text-base leading-6 ">
-            Web designer from USA, California. I have rich experience in web
-            site design and building, also I am good at wordpress. I love to
-            talk with you about our unique.
+          I'm a dedicated software engineer with a profound passion for the convergence of Data Analysis, Web3, and Full Stack Web Development.
+           With a diverse skill set and an unwavering work ethic, I'm driven by my relentless pursuit of personal and professional growth. 
+           I am excited to be part of dynamic teams and make a significant impact in the field of technology and management.
+           In a rapidly evolving tech landscape, I stay ahead by keeping my finger on the pulse of the latest industry trends and emerging skills.
           </p>
         </div>
       </div>
@@ -17,19 +18,11 @@ const AboutMe = () => {
         <ul className="flex flex-col gap-1">
           <li className="aboutRightLi">
             <span className="aboutRightLiSpan">Age:</span>
-            24
+            22
           </li>
           <li className="aboutRightLi">
             <span className="aboutRightLiSpan">Residence:</span>
-            USA
-          </li>
-          <li className="aboutRightLi">
-            <span className="aboutRightLiSpan">Freelance:</span>
-            Available
-          </li>
-          <li className="aboutRightLi">
-            <span className="aboutRightLiSpan">Address:</span>
-            California, USA
+            New Delhi, India
           </li>
         </ul>
       </div>
